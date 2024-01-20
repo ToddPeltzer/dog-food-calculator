d432800901842b9dae84ef43d51a8f82cb473977
Todd Peltzer <toddpeltzer14@gmail.com>
2024-01-19 17:15:09 -0700

    Initial page route to calculator is up/routes are working

---
+++ app/calculator/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function calculator() {
+  return (
+    <div>Calculator Page</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-
+      <Link href="/calculator">Click here to calculate your dogs daily intake</Link>
     </main>
   );
 }

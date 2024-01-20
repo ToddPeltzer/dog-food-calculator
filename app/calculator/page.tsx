6445ba768224a0c7e6bcda75dd58a14abbadd2a4
Todd Peltzer <toddpeltzer14@gmail.com>
2024-01-19 20:43:43 -0700

    Added in Types and changed components to React.FC. added functionality to the form and have output showing with example data.

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import DogFoodForm from '../components/DogFoodForm'
 
-export default function calculator() {
+const Calculator: React.FC = () => {
+
+    const [foodAmountStatement, setFoodAmountStatement] = useState<string>('')
+    
+    const onCalculate = (breed: string, weight: number, age: number) => {
+    
+    let foodAmount: string = '';
+
+    if (weight < 10) {
+        foodAmount = '1lb'
+    } else if (weight < 30) {
+        foodAmount = '2lbs'
+    } else if (weight < 60) {
+        foodAmount = '3lbs'
+    } else if (weight < 100) {
+        foodAmount = '4lbs'
+    } else {
+        foodAmount = '5-8lbs'
+    }
+
+    setFoodAmountStatement(`Your ${breed} needs to eat around ${foodAmount} of food per day`)
+
+  };
+
   return (
     <main className='mt-4 ml-4 mr-4 mb-4'>
         <h1 className='mb-4 text-4xl font-extrabold'>Calculate Your Dogs Dietary Needs</h1>
-        <DogFoodForm />
+        <DogFoodForm onCalculate={onCalculate}/>
+        <h3>Food Amount:</h3>
+        <h4>{foodAmountStatement}</h4>
     </main>
   )
 }
+
+export default Calculator;
--- app/components/DogFoodForm.tsx
+++ app/components/DogFoodForm.tsx
@@ -1,12 +1,32 @@
 "use client"
 import React, { useState } from 'react'
 
-export default function DogFoodForm() {
+// types to be used for the inputs in the form
+interface DogFormProps {
+    onCalculate: (breed: string, weight: number, age: number) => void;
+}
 
-    const [breed, setBreed] = useState('')
-    const [weight, setWeight] = useState('')
-    const [age, setAge] = useState('')
+// react functional component passing in the types (defined above) and then passing the onCalculate function so we can set the parameters from the input fields
+const DogFoodForm: React.FC<DogFormProps> = ({ onCalculate }) => {
 
+    const [breed, setBreed] = useState<string>('')
+    const [weight, setWeight] = useState<number>(0)
+    const [age, setAge] = useState<number>(0)
+
+    const handleCalculate = () => {
+        // when clicking calculate button we pass along the updated dog information to our calulation function
+        onCalculate(breed, weight, age);
+    };
+
+    const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/[^0-9]/g, ''); // Allow only numeric characters
+        setWeight(value || ''); // Set the value or an empty string if it's not a number
+    };
+
+    const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value.replace(/[^0-9]/g, '');
+        setAge(value || '');
+    };
 
   return (
     <form>
@@ -17,21 +37,28 @@ export default function DogFoodForm() {
             value={breed}
             onChange={(e) => setBreed(e.target.value)}
         />
+
         <label htmlFor='weight'>Weight (lbs):</label>
         <input
-            type='number'
+            type='text'
             id='weight'
             value={weight}
-            onChange={(e) => setWeight(e.target.value)}
+            onChange={handleWeightChange}
         />
+
         <label htmlFor='age'>Age:</label>
         <input
-            type='number'
+            type='text'
             id='age'
             value={age}
-            onChange={(e) => setAge(e.target.value)}
+            onChange={handleAgeChange}
         />
-        <button type='button'>Calculate</button>
+
+        <div className='mt-5 mb-5'>
+            <button type='button' onClick={handleCalculate}>Calculate</button>
+        </div>
     </form>
   )
 }
+
+export default DogFoodForm;

d867d4ae2184d8735c9dd1b3465afe3806281ce2
Todd Peltzer <toddpeltzer14@gmail.com>
2024-01-19 17:32:30 -0700

    set up initial form labels and inputs with states for each

--- app/components/DogFoodForm.tsx
+++ app/components/DogFoodForm.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 export default function DogFoodForm() {
+
+    const [breed, setBreed] = useState('')
+    const [weight, setWeight] = useState('')
+    const [age, setAge] = useState('')
+
+
   return (
-    <form>DogFoodForm</form>
+    <form>
+        <label htmlFor='breed'>Breed:</label>
+        <input
+            type='text'
+            id='breed'
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+        />
+        <label htmlFor='weight'>Weight (lbs):</label>
+        <input
+            type='number'
+            id='weight'
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+        />
+        <label htmlFor='age'>Age:</label>
+        <input
+            type='number'
+            id='age'
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+        />
+        <button type='button'>Calculate</button>
+    </form>
   )
 }
